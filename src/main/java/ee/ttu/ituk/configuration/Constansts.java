7a69a3fe7b8fe75a690d1c6218c244292313b870
Johannes Kümmel <jkymmel@gmail.com>
2017-03-10 00:14:06 +0200

    Fixed publicly available API key

--- src/main/java/ee/ttu/ituk/configuration/Constansts.java
+++ src/main/java/ee/ttu/ituk/configuration/Constansts.java
@@ -1,11 +1,14 @@
 package ee.ttu.ituk.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class Constansts {
     public static final String HOUR_AVERAGE_MODE = "dswrfsfc_1_Hour_Average";
     public static final String COUNT = "120";
     public static final String TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
     public static final String BASE_URL = "https://api.planetos.com/v1/datasets/noaa_gfs_pgrb2_global_forecast_recompute_0.25degree/point?origin=dataset-details";
-    public static final String API_KEY = "7721a974ff4e44808149e1e22812dc17";
+    @Value("api-key")
+    public String API_KEY;
     public static final String HEADER_ACCEPT = "Accept";
     public static final String HEADER_VALUE_APPLICATION_JSON = "application/json charset=utf-8";
 
