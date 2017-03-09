f0bd5cd1c342cbe71b2d868eec2d89b39e958170
Johannes Kümmel <jkymmel@gmail.com>
2017-03-09 20:04:01 +0200

    Init

---
+++ src/main/java/ee/ttu/ituk/ProjectSolApplication.java
@@ -0,0 +1,12 @@
+package ee.ttu.ituk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSolApplication.class, args);
+	}
+}
