99958086292bd158e25a0f85d42cafd92fba1c8e
Johannes Kümmel <jkymmel@gmail.com>
2017-03-09 23:28:43 +0200

    Added dependencies

--- src/main/java/ee/ttu/ituk/ProjectSolApplication.java
+++ src/main/java/ee/ttu/ituk/ProjectSolApplication.java
@@ -1,9 +1,11 @@
 package ee.ttu.ituk;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class ProjectSolApplication {
 
 	public static void main(String[] args) {
