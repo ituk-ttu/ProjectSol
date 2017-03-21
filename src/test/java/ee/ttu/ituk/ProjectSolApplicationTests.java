f5e5c69faab651f1f23eb3bdce0f27ec06519b7f
Ilja Samoilov <ilsamoilov@gmail.com>
2017-03-21 23:25:34 +0200

    Fixed critical error in code

--- src/test/java/ee/ttu/ituk/ProjectSolApplicationTests.java
+++ src/test/java/ee/ttu/ituk/ProjectSolApplicationTests.java
@@ -1,16 +1,29 @@
 package ee.ttu.ituk;
 
+import ee.ttu.ituk.data.ResponseData;
+import ee.ttu.ituk.service.CalculationService;
+import ee.ttu.ituk.service.PlanetOSRequestHandler;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+import static junit.framework.TestCase.assertTrue;
+
 @SpringBootTest
 public class ProjectSolApplicationTests {
 
+	@Autowired
+	CalculationService calculationService;
+
+	@Autowired
+	PlanetOSRequestHandler planetOSRequestHandler;
+
 	@Test
-	public void contextLoads() {
+	public void testPlanetOSRequest() {
+		ResponseData responseData = planetOSRequestHandler.performRequest("45","45");
+		assertTrue()
 	}
 
 }
