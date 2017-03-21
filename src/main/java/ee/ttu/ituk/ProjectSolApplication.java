9e81cd6063542cb7298983b05382a84058565117
Johannes Kümmel <jkymmel@gmail.com>
2017-03-22 00:09:17 +0200

    Fixed indentation

--- src/main/java/ee/ttu/ituk/ProjectSolApplication.java
+++ src/main/java/ee/ttu/ituk/ProjectSolApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class ProjectSolApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjectSolApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSolApplication.class, args);
+    }
 }
