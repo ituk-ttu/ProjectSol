9654f9ee92f0e64b7bcd5b6e0efed30f68df54c0
Johannes Kümmel <jkymmel@gmail.com>
2017-03-22 01:46:47 +0200

    Implemented chart drawing

--- src/main/resources/static/assets/js/controllers/graphingController.js
+++ src/main/resources/static/assets/js/controllers/graphingController.js
@@ -1,24 +1,31 @@
 app.controller("graphingController", ["$q", "$scope", "wattageService", function ($q, $scope, wattageService) {
     $scope.loading = false;
-    $scope.result = null;
+    $scope.timestamps = [];
+    $scope.wattages = [[]];
     $scope.error = false;
+    $scope.result = false;
     $scope.panelSettings = {
         inclination: "",
         angleRelativeToMedians: "",
         latitude: "",
         longitude: ""
     };
-
+    $scope.colors = ["#18BC9C"];
 
     $scope.calculate = function (panelSettings) {
         $scope.loading = true;
         $scope.error = false;
-        wattageService.getWattage(panelSettings).then(function (wattage) {
-            $scope.result = wattage;
+        $scope.hasResult = false;
+        wattageService.getWattage(panelSettings).then(function (result) {
+            $scope.timestamps = result.data.timeList;
+            $scope.wattages = [result.data.solarPowerList];
+            $scope.hasResult = true;
             $scope.loading = false;
         }).catch(function (wattage) {
+            $scope.timestamps = [];
+            $scope.wattages = [[]];
             $scope.error = true;
             $scope.loading = false;
         });
-    }
+    };
 }]);
