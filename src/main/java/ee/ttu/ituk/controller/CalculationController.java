e8058fe488707ea7a0edb3b8c854c3b50fcf2dd8
Ilja Samoilov <ilsamoilov@gmail.com>
2017-03-22 00:03:06 +0200

    RequestData fix

--- src/main/java/ee/ttu/ituk/controller/CalculationController.java
+++ src/main/java/ee/ttu/ituk/controller/CalculationController.java
@@ -1,16 +1,15 @@
 package ee.ttu.ituk.controller;
 
 import ee.ttu.ituk.data.GraphData;
+import ee.ttu.ituk.data.RequestData;
 import ee.ttu.ituk.data.ResponseData;
 import ee.ttu.ituk.service.CalculationService;
 import ee.ttu.ituk.service.PlanetOSRequestHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 
@@ -23,13 +22,10 @@ public class CalculationController {
     @Autowired
     PlanetOSRequestHandler planetOSRequestHandler;
 
-    @RequestMapping(value = "/calculate", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public @ResponseBody GraphData calculate(@RequestParam(name = "inclination") String inclination,
-                                              @RequestParam(name = "angleRelativeToMedians") String angleRelativeToMedians,
-                                              @RequestParam(name = "longitude") String longitude,
-                                              @RequestParam(name = "latitude") String latitude) {
-        ResponseData responseData = planetOSRequestHandler.performRequest(latitude, longitude);
-        return calculationService.calculateRealSolarPower(responseData, new BigDecimal(inclination), new BigDecimal(angleRelativeToMedians));
+    @RequestMapping(value = "calculate", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public @ResponseBody GraphData calculate(@RequestBody RequestData requestData) {
+        ResponseData responseData = planetOSRequestHandler.performRequest(requestData.getLatitude(), requestData.getLongitude());
+        return calculationService.calculateRealSolarPower(responseData, new BigDecimal(requestData.getInclination()), new BigDecimal(requestData.getAngleRelativeToMedians()));
     }
 
 }
